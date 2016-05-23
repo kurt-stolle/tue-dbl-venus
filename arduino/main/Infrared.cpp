3b0384dbe8f64dc4fab2820946bca3161706ae45
timmstek <stek.tim@gmail.com>
2016-05-24 00:20:16 +0200

    Some code for the IR sensors
    Does complie, but we should discuss what exactly is the input for this
    function

--- arduino/main/Infrared.cpp
+++ arduino/main/Infrared.cpp
@@ -1,5 +1,8 @@
 #include "Infrared.h"
 
+#define GRAY_THOLD 15
+#define WHITE_THOLD 200
+
 Infrared::Infrared(pin measurePin) {
   this->measurePin = measurePin;
 }
@@ -8,6 +11,18 @@ Infrared::Color Infrared::GetColor() {
   // TODO
 
   // measure pin and return color (Infrared::WHITE, Infrared::GRAY, Infrared::BLACK)
-  
+  int measurement = 1;
+  if (measurement < GRAY_THOLD) {
+    return Infrared::BLACK;
+  }
+
+  if (measurement >= GRAY_THOLD && measurement < WHITE_THOLD) {
+    return Infrared::GRAY;
+  }
+
+  if (measurement >= WHITE_THOLD) {
+    return Infrared::WHITE;
+  }
 }
+
 
